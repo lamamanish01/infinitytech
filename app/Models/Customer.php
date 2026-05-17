6f59edafe7896cbcb2c3e8cd0d5e3a71c825895c
lamamanish01 <lamamanish166@gmail.com>
2026-05-17 21:22:16 +0545

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -46,7 +46,7 @@ class Customer extends Model
 
     public function internetPlan()
     {
-        return $this->belongsTo(InternetPlan::class, 'id');
+        return $this->belongsTo(InternetPlan::class, 'internet_plan_id');
     }
 
     public function user()
