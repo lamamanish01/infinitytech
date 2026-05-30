f1c5552f3b8b6c4815ba8c39443b560d5ea3dea1
lamamanish01 <lamamanish166@gmail.com>
2026-05-30 23:15:48 +0545

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->setupGates();
         $this->setupPagination();
-        $this->shareGlobalViewData();
+        // $this->shareGlobalViewData();
     }
 
     /**
