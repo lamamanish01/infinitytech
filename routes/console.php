ba7cd74d92ae80b749a17df9764eb83de64fb5c1
lamamanish01 <lamamanish166@gmail.com>
2026-07-04 19:59:06 +0545

    update

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('cron:run-dynamic')
-    ->everyMinute()
-    ->timezone('Asia/Kathmandu')
-    ->withoutOverlapping();
+    ->everyMinute();
 
